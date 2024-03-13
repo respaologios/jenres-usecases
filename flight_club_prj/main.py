551965470f785cec5bbaf54e59da62d4f7bf5db4
mfieromonte <martina.fieromonte@res-it.com>
2024-03-13 09:31:57 +0100

    flight_prj

---
+++ flight_club_prj/main.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
+
+ORIGIN_CITY_IATA = "LON"
+
+data_manager = DataManager()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+sheet_data = data_manager.get_destination_data()
+
+if sheet_data[0]["iataCode"] == "":
+    city_names = [row["city"] for row in sheet_data]
+    data_manager.city_codes = flight_search.get_destination_codes(city_names)
+    data_manager.update_destination_codes()
+    sheet_data = data_manager.get_destination_data()
+
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data}
+
+tomorrow = datetime.now() + timedelta(days=1)
